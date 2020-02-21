de790ff1af346f85537039a85941af354833d5ac
Daisy Sanchez <sanchez.daisy86@gmail.com>
2020-02-20 16:33:28 -0800

    Commented code

--- diffNetB.R
+++ diffNetB.R
@@ -2,10 +2,9 @@ library(netdiffuseR)
 
 # reads in a csv file
 # must  be formatted as 1st column: year, following columns of authors in that reference
-
   CSV = read.csv(file.choose(), header = FALSE, stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
 
-#creates new dataframe, a list of authors and the year they appeared
+  #creates new dataframe, a list of authors and the year they appeared
   authorYear = data.frame(matrix(ncol = 3, nrow = 0), stringsAsFactors = FALSE)
   
   #renames columns in authorYear
@@ -17,55 +16,58 @@ library(netdiffuseR)
   
   #adds authors to authorYear
   #nested for loops search both rows and columns to ensure all authors are read into authorYear
-  count = 0
-  year = CSV[1,1]
+  count = 0 #counter for int id assignment of authors
+  year = CSV[1,1] #keeps track of year to update/reset count
   for(i in 1:nrow(CSV)){
+    
     for(j in 2:ncol(CSV)){ 
       #checks if cell is a comparable string
       if(CSV[i,j] != ' ' && CSV[i,j] != '' &&  !is.null(CSV[i,j]) && !is.na(CSV[i,j])){
-        if(year != CSV[i,1]){
-          count = 0;
-          year = CSV[i,1]
-        }
-        auth = CSV[i,j]
-        auth = paste('^', auth, '$', sep = '') #raps auth name in regex
         
-          if(length(grep(auth, authorYear$author, ignore.case = TRUE)) != 0){ #if exists in authyear look for id
-            temp = grep(auth, authorYear$author, ignore.case = TRUE)
-            min = which.min(temp)
-            minIndex = temp[min]
-            id = authorYear[minIndex, 3]
+        if(year != CSV[i,1]){ #checks if the same year as prev
+          count = 0; #if a new year, resets count
+          year = CSV[i,1] #updates year
+        }
+ 
+        auth = CSV[i,j] #records current author name
+        auth = paste('^', auth, '$', sep = '') #raps auth name in regex so that it can be compared
+        
+          if(length(grep(auth, authorYear$author, ignore.case = TRUE)) != 0){ #if auth exists in authyear look for id
+            temp = grep(auth, authorYear$author, ignore.case = TRUE) #returns the indices at which the name appear
+            min = which.min(temp) #grabs the first index
+            minIndex = temp[min] #grabs the row number
+            id = authorYear[minIndex, 3] #sets the id to exisiting id so that it can be assigned correctly
           } else { #if auth does not exist in authYear calc id
-            id = CSV[i, 1] * 1000 + count
-            count = count + 1
+            id = CSV[i, 1] * 1000 + count #create a new id 
+            count = count + 1 #update count to avoid assignment of same id to multiple authors
           }
         
+        #creates and adds new row authorYear
         newRow = data.frame(author = CSV[i,j], year = CSV[i,1], id, stringsAsFactors = FALSE)
         authorYear = rbind(authorYear, newRow)
      
-      } else {
+      } else { #if string at csv[i,j] is not a comparable string, the last author in this reference has already been read and it is safe to continue to the next row
         break
       }
     
-    }
-  }
+    } #end of coloumn file traversal
+  } #end of csv file traversal
   
   #removes temporary row added at the beginning
   authorYear <- authorYear[-c(1),]
   
   #now, a dataframe of authors, their year of publication, and their id has been created
+  
   #the next step is to create an edgelist out of their id's
   
-  #deletes variables that will no be used again
-  rm(newRow, auth, count, i, id, j, min, minIndex, temp, year)
-
-#sorts dataframe by year in ascending order
-#authorYear = authorYear[order(authorYear$year),]
-
-#creates edgelist from authorYear dataframe
+  #deletes variables that will no longer be used again
+  rm(auth, count, i, id, j, min, minIndex, temp, year, newRow)
+ 
+#CREATES EDGELIST FROM AUTHORYEAR DATAFRAME
 
   #initializes authEdgelist
   authEdgelist = data.frame(matrix(ncol = 2, nrow = 0))
+  
   #sets column names
   colnames(authEdgelist) = c("alter", "ego")
   
@@ -73,67 +75,69 @@ library(netdiffuseR)
   #first for loop for parsing through authorYear, and adding every author to alter
   
   for(i in 1:nrow(authorYear)){
-    authAlter = authorYear[i,3]
+    authAlter = authorYear[i,3] #stores current author and year
     year = authorYear[i,1]
-  
-    j = i+1
-    #second for loop used to add ego authors
-    for(j in 1:nrow(authorYear)){
-      if(authorYear[j,1] == (year+1)){
-        authEgo = authorYear[j,3]
+    
+    j = i+1 #sets j to row after current
+    
+    for(j in 1:nrow(authorYear)){ #second for loop used to add ego authors
+      if(authorYear[j,1] == (year+1)){ #if current author published the year after saved author
+        authEgo = authorYear[j,3] #then save author as ego
         
-        newRow = data.frame(alter = authAlter, ego = authEgo)
-        authEdgelist = rbind(authEdgelist, newRow)
-      } 
-      
-    }
-  }
+        newRow = data.frame(alter = authAlter, ego = authEgo) #create new row of saved author, current author
+        authEdgelist = rbind(authEdgelist, newRow) #adds newrow to edgelist
+      } #endif
+       
+    } #end inner loop
+  } #end outer loop
   
-  #deletes variables that will no be used again
-  rm(i, j, year, authAlter, authEgo, newRow)
+  #deletes variables that will no longer be used again
+  rm(authAlter, authEgo, i, j, year, newRow)
   
-  temp <- edgelist_to_adjmat(
-    edgelist = authEdgelist[,1:2], 
-    undirected = FALSE, 
-    t=nrow(authEdgelist)
+  
+  
+#CREATES DIFFUSION NETWORK
+  temp <- edgelist_to_adjmat( #creates adjacency matrix
+    edgelist = authEdgelist[,1:2], #defines the edgelist as all the rows of authEdgelist, and its first two columns
+    undirected = FALSE, #defines edgelist as directed
+    t = nrow(authEdgelist) #defines the number of time periods as the number of rows
   )
   
-  adjmat <- edgelist_to_adjmat(
-    edgelist = authEdgelist[,1:2],
-    undirected = FALSE,
-    t=nrow(temp[[1]])
+  adjmat <- edgelist_to_adjmat( #creates adjacency matrix
+    edgelist = authEdgelist[,1:2], #defines the edgelist as all the rows of authEdgelist, and its first two columns
+    undirected = FALSE, #defines edgelist as directed
+    t=nrow(temp[[1]]) #defines the number of time periods as the number of rows 
   )
   
   rm(temp)
   
-  toa <- data.frame(matrix(ncol = 1, nrow = 0))
+  toa <- data.frame(matrix(ncol = 1, nrow = 0)) #creates toa list
+  colnames(toa) <- c("toa") #renames column
   
-  colnames(toa) <- c("toa")
-  
-  for(i in 1:nrow(adjmat[[1]])){
+  for(i in 1:nrow(adjmat[[1]])){ #populates the toa list with num rows of adjmat[[1]]
     toa[i,1] = i
   }
   
-  diffnet <- as_diffnet( graph=adjmat, toa=toa$toa)
+  diffnet <- as_diffnet( graph=adjmat, toa=toa$toa) #
   
   timePeriod <- 1
   timeRange <- nrow(adjmat[[1]])
   
-  cat("Select a time period to plot between 1 and", timeRange, ". Input 0 to quit", "\n")
-  while(timePeriod!=0){
+  cat("Select a time period to plot between 1 and ", timeRange, ". Input 0 to quit", "\n")
+  while(timePeriod != 0){
     timePeriod <- as.integer(readline())
     
     
     if(timePeriod == 0) break
     
     #checks for valid time period
-    if(timePeriod<1 || timePeriod>timeRange){
-      cat("Invalid time period. Please enter a number between 1 and", timeRange, "or 0 to quit", "\n")
+    if(timePeriod < 1 || timePeriod > timeRange){
+      cat("Invalid time period. Please enter a number between 1 and ", timeRange, " or 0 to quit", "\n")
       next
     }
     
     plot(diffnet, t=timePeriod)
     
-    cat("Select another time period to plot between 1 and", timeRange, "input 0 to quit", "\n")
+    cat("Select another time period to plot between 1 and ", timeRange, " input 0 to quit", "\n")
   }
   
